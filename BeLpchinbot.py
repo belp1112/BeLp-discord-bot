f24e02c1da05ca3865eff6017729d90e92185b71
BeLp <koidekko1112@gmail.com>
2021-05-16 14:18:35 +0900

    first commit

---
+++ BeLpchinbot.py
@@ -0,0 +1,80 @@
+from asyncio import TimeoutError
+from os import getenv
+
+import discord
+from requests import get
+from unicodedata import name
+from riot import return_collect_champion_name, retrieve_championdata, return_skinlist, shape_to_discordmsg
+
+TOKEN = getenv('BeLChinBotTOKEN')
+
+client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if not message.content.startswith('!lol'):
+        return
+
+    #sender_id = message.author.id
+    #channel_id = message.channel.id
+    message_split = message.content.split( )
+    #エラー処理 チャンピオン名を入力しなかった場合
+    if len(message_split) < 2:
+        await message.reply('チャンピオン名を入力してください')
+        return
+
+    if message_split[1] == 'patch':
+        patch_ver = list(get('https://ddragon.leagueoflegends.com/api/versions.json').json())[0]
+        await message.reply(patch_ver)
+    else:
+        champion_name = message_split[1]
+        language = language_judge(champion_name)
+        if language == 'en_US':
+            champion_name = champion_name.capitalize()
+        id_champion_name = return_collect_champion_name(language,champion_name)
+        champion_data = retrieve_championdata(id_champion_name, language)
+        if type(champion_data) is not dict:
+            await message.reply(champion_data)
+            return
+        else:
+            skin_list = return_skinlist(champion_data, id_champion_name)
+            shaped_skinlist = shape_to_discordmsg(skin_list)
+            await message.reply(shaped_skinlist)
+
+        def check(m) -> bool:
+            return True
+
+        try:
+            msg = await client.wait_for('message', check=check, timeout=90)
+            nums = msg.content.split(' ')
+            skin_num = []
+            for i in nums:
+                skin_num.append(skin_list[int(i) - 1]['num'])
+            skin_num_list = []
+            for i in skin_num:
+                skin_num_list.append(
+                "http://ddragon.leagueoflegends.com"
+                f"/cdn/img/champion/splash/{id_champion_name}_{i}.jpg"
+                )
+            for i in skin_num_list:
+                await msg.reply(i)
+        except TimeoutError:
+            await message.reply("キャンセルされました")
+        return
+
+
+def language_judge(champion_name):
+    '''文字の最初の一文字を判定して言語名を返す'''
+    jadged_language = name(champion_name[0]).split(' ')
+    if jadged_language[0] == 'LATIN':
+        return 'en_US'
+    elif jadged_language[0] == 'KATAKANA':
+        return 'ja_JP'
+    elif jadged_language[0] == 'HANGUL':
+        return 'ko_KR'
+
+
+client.run(TOKEN)
---
+++ riot.py
@@ -0,0 +1,60 @@
+from asyncio.windows_events import ERROR_CONNECTION_REFUSED
+from requests import get
+
+
+
+def return_collect_champion_name(language:str , champion_name:str) ->list:
+    version = list(get('https://ddragon.leagueoflegends.com/api/versions.json').json())[0]
+    resp = get(f'http://ddragon.leagueoflegends.com/cdn/{version}/data/{language}/champion.json').json()
+    all_champion_data_list = [i for i in resp['data']]
+    all_champion_id_list   = [resp['data'][i]['id'] for i in all_champion_data_list]
+    all_champion_name_list = [resp['data'][i]['name'] for i in all_champion_id_list]
+    all_champion_name_dict_list = dict(zip(all_champion_name_list, all_champion_data_list))
+    try:
+        return all_champion_name_dict_list[champion_name]
+    except:
+        KeyError
+#print(return_collect_champion_name('en_US', 'katarina'))
+
+def retrieve_championdata(id_champion_name, language:str,) -> dict:
+    """チャンピオン名を受け取ってそのチャンピオンのjsonまたはエラーメッセージ(str)を返す"""
+    version = list(get('https://ddragon.leagueoflegends.com/api/versions.json').json())[0]
+    resp = get(f'http://ddragon.leagueoflegends.com/cdn/{version}/data/{language}/champion/{id_champion_name}.json')
+    if resp.status_code != 200: #エラー処理 レスポンスコードが200では無かった場合
+        error = 'スペルが間違っているか存在しないチャンピオンです'
+        return error
+    championdata = resp.json()
+    return championdata
+
+
+def return_skinlist(championdata: dict, championname: str) -> list:
+    '''受け取ったチャンピオンの全てのスキン名とスキン番号を辞書型にし、リストに入れて返す'''
+    skin_list = []
+#   skin_list = [{skin['name']: skin['num']} for skin in championdata['data'][championname]['skins']]
+    for skin in championdata['data'][championname]['skins']:
+        skin_list.append({'name': skin['name'], 'num':skin['num']})
+    return skin_list
+
+
+def shape_to_discordmsg(skin_list: list) ->str:
+    temp = []
+    for idx in range(len(skin_list)):
+        temp.append(f"`{idx + 1}`: {skin_list[idx]['name']}")
+    temp.append('スペース区切りで番号を入力してください')
+    return '\n'.join(temp)
+
+
+
+#skin_list = return_skinlist(retrieve_championdata('Katarina'), 'Katarina')
+#print(shape_to_discmsg(skin_list))
+
+
+# children : [{'カタリニャ': 4,}, {}, {}]
+
+# !bel {champion_name}
+#     画像ナンバーの list を返す
+# !bel {champion_name} [num]
+#     画像 URL を返す
+
+# ["data"][{champion_name}]["id"] == "英語名"
+# ["data"][{champion_name}]["name"] == "日本語名"
